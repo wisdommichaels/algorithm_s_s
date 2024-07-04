8433d980a748454e443a36067418d8c72a1deab3
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-04 11:07:01 +0100

    added a read me file

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function insertionSort(array) {
-    for(let i = 1; i < array.length; i++) {
+// * Implementation of the Insertion Sort algorithm.
+
+ function insertionSort(array) {
+        // Start from the second element (index 1) since the first element is trivially sorted.
+        for (let i = 1; i < array.length; i++) {
+            // Store the current element to be inserted into the sorted portion of the array.
             let key = array[i];
+            
+            // Initialize the index of the previous element.
             let j = i - 1;
+            
+            // Shift elements greater than the key to the right to make room for the key.
             while (j >= 0 && array[j] > key) {
-                    array[j + 1] = array[j];
-                    j = j - 1;
+                // Move the element at index j to the right.
+                array[j + 1] = array[j];
+                // Decrement j to continue shifting elements.
+                j = j - 1;
             }
+            
+            // Insert the key into its correct position.
             array[j + 1] = key;
+        }
+        
+        // Return the sorted array.
+        return array;
     }
-    return array;
-}
+   
